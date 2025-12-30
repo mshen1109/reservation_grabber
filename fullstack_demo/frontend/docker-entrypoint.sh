42b387a44c8c9ace484d1650709456fdbef2382c
miaomiao shen <miamia09@gmail.com>
2025-12-30 11:40:49 -0700

    refactor: Move fullstack_demo to separate repository
    Fullstack demo has been moved to its own repository:
    https://github.com/mshen1109/fullstack_demo
    
    This repository now contains only the reservation grabber scripts.

--- fullstack_demo/frontend/docker-entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Substitute environment variables in nginx config
-envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-nginx -g 'daemon off;'
